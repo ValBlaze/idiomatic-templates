442666094b21839a3f484402b8e4707e6f1ab616
Val Blaze <valblazeyt@gmail.com>
2026-05-07 04:58:23 -0500

    fix: added content for devshell template

---
+++ devshell/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Minimal flake with a devShell";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              # Build Tools
+            ];
+
+            buildInputs = with pkgs; [
+              # Runtime Dependencies
+            ];
+          };
+        }
+      );
+    };
+}
